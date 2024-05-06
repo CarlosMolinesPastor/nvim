d2d1881bf078bd25d93d4354d3048dfbcf00b16d
CarlosMolinesPastor <karlinux47@gmail.com>
2024-05-07 00:38:09 +0200

    Added Archives nvim

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "craftzdog/solarized-osaka.nvim",
+    branch = "osaka",
+    lazy = true,
+    priority = 1000,
+    opts = function()
+      return {
+        transparent = false,
+      }
+    end,
+  },
+
+  { "ellisonleao/gruvbox.nvim" },
+
+  { "nxstynate/oneDarkPro.nvim" },
+
+  { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
+
+  {
+    "navarasu/onedark.nvim",
+    style = "darker",
+  },
+
+  -- Configure LazyVim to load gruvbox
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
